6fa9e047af56f63aa53f6389cdd8ed8901b06743
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-06-01 16:25:44 +0200

    CitySearch test

---
+++ src/__tests__/CitySearch.test.js
@@ -0,0 +1,28 @@
+import { render } from "@testing-library/react";
+import CitySearch from "../components/CitySearch";
+import userEvent from "@testing-library/user-event";
+
+describe("<CitySearch /> component", () => {
+  test("renders text input", () => {
+    const CitySearchComponent = render(<CitySearch />);
+    const cityTextBox = CitySearchComponent.queryByRole("textbox");
+    expect(cityTextBox).toBeInTheDocument();
+    expect(cityTextBox).toHaveClass("city");
+  });
+
+  test("suggestions list is hidden by default", () => {
+    const CitySearchComponent = render(<CitySearch />);
+    const suggestionList = CitySearchComponent.queryByRole("list");
+    expect(suggestionList).not.toBeInTheDocument();
+  });
+
+  test("renders a list of suggestions when city textbox gains focus", async () => {
+    const CitySearchComponent = render(<CitySearch />);
+    const user = userEvent.setup();
+    const cityTextBox = CitySearchComponent.queryByRole("textbox");
+    await user.click(cityTextBox);
+    const suggestionList = CitySearchComponent.queryByRole("list");
+    expect(suggestionList).toBeInTheDocument();
+    expect(suggestionList).toHaveClass("suggestions");
+  });
+});
