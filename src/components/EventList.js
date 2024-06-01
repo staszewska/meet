d52f0bf4f13e5295891d1ce176eb75032a0576c3
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-06-01 09:25:05 +0200

    EventList code added

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,5 +1,11 @@
-const EventList = () => {
-  return <ul id="event-list"></ul>;
+import Event from "./Event";
+
+const EventList = ({ events }) => {
+  return (
+    <ul id="event-list">
+      {events ? events.map((event) => <Event event={event} />) : null}
+    </ul>
+  );
 };
 
 export default EventList;
