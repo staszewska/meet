3770d59b89889583d5af58e81694909590c8c4b9
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-06-02 22:55:55 +0200

    Event.js added

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,5 +1,26 @@
-const Event = () => {
-  return <li></li>;
+import { useState } from "react";
+
+const Event = ({ event }) => {
+  const [showDetails, setShowDetails] = useState(false);
+  return (
+    <li className="event" key={event.id}>
+      <h2>{event.summary}</h2>
+      <p>{event.created}</p>
+      <p>{event.location}</p>
+      <button
+        className="showDetailsButton"
+        onClick={() => setShowDetails(!showDetails)}
+      >
+        {showDetails ? "Hide Details" : "Show Details"}
+      </button>
+      {showDetails ? (
+        <div className="details">
+          <h3>Event Details</h3>
+          <p>{event.description}</p>
+        </div>
+      ) : null}
+    </li>
+  );
 };
 
 export default Event;
