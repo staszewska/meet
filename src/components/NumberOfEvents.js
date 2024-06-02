0d31bf28e129aef3d64538f8f249c0d6b8687245
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-06-02 23:09:26 +0200

    component rendered

---
+++ src/components/NumberOfEvents.js
@@ -0,0 +1,31 @@
+const NumberOfEvents = ({ setCurrentNOE, setErrorAlert }) => {
+  const handleInputChanged = (event) => {
+    const value = event.target.value;
+
+    // Alerts
+    let errorText;
+    if (value <= 0) {
+      errorText = "The number must be higher than 0.";
+    } else {
+      errorText = "";
+    }
+
+    setCurrentNOE(value);
+    setErrorAlert(errorText);
+  };
+
+  return (
+    <div id="number-of-events">
+      <label htmlFor="number-of-events-input">Number of Events: </label>
+      <input
+        type="number"
+        id="number-of-events-input"
+        className="number-of-events-input"
+        defaultValue={32}
+        onChange={handleInputChanged}
+      />
+    </div>
+  );
+};
+
+export default NumberOfEvents;
