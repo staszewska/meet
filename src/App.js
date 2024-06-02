ed784c535c0e87a1bd58a37c045caaf1fdcd0c77
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-06-02 23:09:01 +0200

    NumberOfEvents component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
+import NumberOfEvents from "./components/NumberOfEvents";
 
 const App = () => {
   return (
     <div className="App">
       <CitySearch />
       <EventList />
+      <NumberOfEvents />
     </div>
   );
 };
