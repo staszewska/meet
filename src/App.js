ba8003c9ef7f44887431a259634db73a7b0b79d1
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-06-04 16:21:59 +0200

    data fetched, state added, use Effect used

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
 import NumberOfEvents from "./components/NumberOfEvents";
+import { getEvents } from "./api";
 
 const App = () => {
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    setEvents(allEvents.slice(0, currentNOE));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <CitySearch />
-      <EventList />
+      <EventList events={events} />
       <NumberOfEvents />
     </div>
   );
