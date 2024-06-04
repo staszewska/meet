02ca7627c0777c6ee0a9e7210932509109d2feff
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-06-04 16:21:26 +0200

    act imported and added

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,6 +1,8 @@
-import { render } from "@testing-library/react";
+import { render, within, waitFor } from "@testing-library/react";
 import EventList from "../components/EventList";
 import { getEvents } from "../api";
+import { act } from "react";
+import App from "../App";
 
 describe("<EventList /> component", () => {
   let EventListComponent;
@@ -14,9 +16,27 @@ describe("<EventList /> component", () => {
 
   test("renders correct number of events", async () => {
     const allEvents = await getEvents();
-    EventListComponent.rerender(<EventList events={allEvents} />);
+    act(() => {
+      EventListComponent.rerender(<EventList events={allEvents} />);
+    });
     expect(EventListComponent.getAllByRole("listitem")).toHaveLength(
       allEvents.length
     );
+  });
+});
+
+describe("<EventList /> integration", () => {
+  test("renders a list of 32 events when the app is mounted and rendered", async () => {
+    let AppComponent;
+    await act(async () => {
+      // Wrap the rendering and async operations in act
+      AppComponent = render(<App />);
+    });
+    const AppDOM = AppComponent.container.firstChild;
+    const EventListDOM = AppDOM.querySelector("#event-list");
+    await waitFor(() => {
+      const EventListItems = within(EventListDOM).queryAllByRole("listitem");
+      expect(EventListItems.length).toBe(32);
+    });
   });
 });
