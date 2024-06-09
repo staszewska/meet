a3470d6e88898e3b65cf987d7d27135696ffbe40
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-06-09 16:29:56 +0200

    No Access Token Found in localStorage code

--- src/api.js
+++ src/api.js
@@ -21,3 +21,25 @@ export const extractLocations = (events) => {
 export const getEvents = async () => {
   return mockData;
 };
+
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem("access_token");
+
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const response = await fetch(
+        "https://xyu8ad23ra.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
+      );
+      const result = await response.json();
+      const { authUrl } = result;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
+};
