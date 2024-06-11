4a4eea2f6970299f993fd464bb0ab3b32428bc27
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-06-11 23:32:31 +0200

    Feature 3: Specify Number of Events test completed

--- src/features/specifyNumberOfEvents.test.js
+++ src/features/specifyNumberOfEvents.test.js
@@ -1,4 +1,7 @@
 import { loadFeature, defineFeature } from "jest-cucumber";
+import App from "../App";
+import { render, within, waitFor, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 const feature = loadFeature("./src/features/specifyNumberOfEvents.feature");
 
@@ -7,9 +10,20 @@ defineFeature(feature, (test) => {
     given,
     then,
   }) => {
-    given("the user opens the events app", () => {});
+    let AppComponent;
+    given("the user opens the events app", () => {
+      AppComponent = render(<App />);
+    });
 
-    then(/^(\d+) events should be shown by default$/, (arg0) => {});
+    then(/^(\d+) events should be shown by default$/, async (arg0) => {
+      const AppDOM = AppComponent.container.firstChild;
+      const EventListDOM = AppDOM.querySelector("#event-list");
+
+      await waitFor(() => {
+        const EventListItems = within(EventListDOM).queryAllByRole("listitem");
+        expect(EventListItems.length).toBe(32);
+      });
+    });
   });
 
   test("User can change the number of events displayed", ({
@@ -17,13 +31,36 @@ defineFeature(feature, (test) => {
     when,
     then,
   }) => {
-    given("the user opens the events app", () => {});
+    let AppComponent;
+    let AppDOM;
+    const numberOfEvents = 3;
 
-    when("the user selects a different number of events to display", () => {});
+    given("the user opens the events app", () => {
+      AppComponent = render(<App />);
+    });
+
+    when(
+      "the user selects a different number of events to display",
+      async () => {
+        const user = userEvent.setup();
+        AppDOM = AppComponent.container.firstChild;
+        const NumberOfEventsComponent =
+          AppDOM.querySelector("#number-of-events");
+
+        const numberInput = AppDOM.querySelector("#number-of-events-input");
+        // console.log(numberInput);
+        await user.type(numberInput, "{backspace}{backspace}3");
+        expect(numberInput).toHaveValue(numberOfEvents);
+      }
+    );
 
     then(
-      "the number of events displayed should match the user's selection",
-      () => {}
+      "the number of events displayed should match the user`s selection",
+      () => {
+        const EventListDOM = AppDOM.querySelector("#event-list");
+        const EventListItems = within(EventListDOM).queryAllByRole("listitem");
+        expect(EventListItems.length).toBe(numberOfEvents);
+      }
     );
   });
 });
